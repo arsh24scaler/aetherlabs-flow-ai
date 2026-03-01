c598e46408f3551f9335e94fb3e8e1b50314551e
Ubuntu <flow-admin@aetherlabs-flow-compose.azn2lpb51j2upgvdjaswggklte.rx.internal.cloudapp.net>
2026-03-01 21:15:38 +0000

    chore: remove temporary test files causing build failure

--- test-pdf.ts
+++
@@ -1,5 +0,0 @@
-import pdfParse from 'pdf-parse';
-import * as pdfParseStar from 'pdf-parse';
-
-console.log("Default import:", typeof pdfParse, pdfParse);
-console.log("Star import:", typeof pdfParseStar, pdfParseStar);
